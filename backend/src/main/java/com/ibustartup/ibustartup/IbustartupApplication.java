86a44e2237e53febd85a14288472358015261712
seadmasetic <75685855+seomasha@users.noreply.github.com>
2025-04-05 14:15:12 +0200

    FE/BE - Project structure (#1)
    * Adding frontend structure
    
    * adding backend structure

---
+++ backend/src/main/java/com/ibustartup/ibustartup/IbustartupApplication.java
@@ -0,0 +1,13 @@
+package com.ibustartup.ibustartup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbustartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbustartupApplication.class, args);
+	}
+
+}
