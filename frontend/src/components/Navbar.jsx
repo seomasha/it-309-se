dcaf310148bddb5c32218db575fc0defe2a4bce9
Sead Mašetić <75685855+seomasha@users.noreply.github.com>
2025-05-18 13:39:32 +0200

    My profile - FE (#5)
    * Adding api services, global error handler with toast messages and form validation for login
    
    * Bugfix
    
    * Created navbar
    
    * Added My profile page
    
    * FE Styling changes
    
    * Fetching user data

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "../styles/Navbar.css";
+import logo from "../assets/IBUStartupLogo.png";
+import SearchBar from "./SearchBar";
+import { IoMdHome } from "react-icons/io";
+import {
+  FaBriefcase,
+  FaMoneyBillWaveAlt,
+  FaUserFriends,
+  FaBell,
+} from "react-icons/fa";
+import NavIcon from "./NavIcon";
+
+const Navbar = () => {
+  const navIcons = [
+    { id: 1, icon: <IoMdHome size={22} />, title: "Home" },
+    { id: 2, icon: <FaBriefcase size={22} />, title: "Startups" },
+    { id: 3, icon: <FaMoneyBillWaveAlt size={22} />, title: "Investors" },
+    { id: 4, icon: <FaUserFriends size={22} />, title: "Friends" },
+    { id: 5, icon: <FaBell size={22} />, title: "Notifications" },
+  ];
+
+  return (
+    <header className="nav d-flex justify-content-between p-3 px-4 border-bottom align-items-center">
+      <div className="d-flex primary-color">
+        <img src={logo} width={50} height={40} />
+        <SearchBar />
+      </div>
+      <div className="d-flex gap-4 align-items-center">
+        {navIcons.map((item) => (
+          <NavIcon key={item.id} icon={item.icon} title={item.title} />
+        ))}
+        <NavIcon image title="My profile" />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
